819528bd7d6628c8e4940505968f69faf9299b2f
Tyler Fanelli <tfanelli@redhat.com>
2023-10-02 21:19:38 -0400

    include/quic: Add max_idle_timeout to transport params
    max_idle_timeout is used to set the QUIC idle timer.
    
    Signed-off-by: Tyler Fanelli <tfanelli@redhat.com>

--- handshake.c
+++ handshake.c
@@ -843,6 +843,7 @@ static void quic_context_copy_transport_params(struct quic_endpoint *ep,
 	param->max_udp_payload_size = p->max_udp_payload_size;
 	param->ack_delay_exponent = p->ack_delay_exponent;
 	param->max_ack_delay = p->max_ack_delay / NGTCP2_MICROSECONDS;
+	param->max_idle_timeout = p->max_idle_timeout / NGTCP2_MICROSECONDS;
 	param->initial_max_data = p->initial_max_data;
 	param->active_connection_id_limit = p->active_connection_id_limit;
 	param->initial_max_stream_data_bidi_local = p->initial_max_stream_data_bidi_local;
