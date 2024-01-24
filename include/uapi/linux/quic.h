f98c49a4b6b2de9f7de31bc393344d3adf389d8a
Xin Long <lucien.xin@gmail.com>
2024-01-24 14:16:04 -0500

    uapi: add WITH Linux-syscall-note in first line in quic.h
    To keep consistent with other uapi files from kernel tree,
    add WITH Linux-syscall-note in first line in uapi quic.h.
    
    Signed-off-by: Xin Long <lucien.xin@gmail.com>

--- include/uapi/linux/quic.h
+++ include/uapi/linux/quic.h
@@ -1,4 +1,4 @@
-/* SPDX-License-Identifier: GPL-2.0-or-later */
+/* SPDX-License-Identifier: GPL-2.0+ WITH Linux-syscall-note */
 /* QUIC kernel implementation
  * (C) Copyright Red Hat Corp. 2023
  *
