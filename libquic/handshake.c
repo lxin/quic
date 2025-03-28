8b172d3a165d1dfc1b9575a6196fa00ee3247db0
Stefan Metzmacher <metze@samba.org>
2025-03-28 12:17:44 +0100

    libquic: also use poll for the non-blocking sendmsg
    Otherwise we may burn cpu without a need.
    
    Signed-off-by: Stefan Metzmacher <metze@samba.org>

--- libquic/handshake.c
+++ libquic/handshake.c
@@ -708,6 +708,21 @@ int quic_handshake(gnutls_session_t session)
 		while (msg) {
 			quic_log_debug("< Handshake SEND: %u %u", msg->len, msg->level);
 			ret = quic_handshake_sendmsg(sockfd, msg);
+			if ((ret < 0) && (errno == EAGAIN || errno == EWOULDBLOCK)) {
+				struct pollfd pfd = {
+					.fd = sockfd,
+					.events = POLLOUT,
+				};
+				int prc;
+
+				prc = poll(&pfd, 1, 1000);
+				if (prc < 0) {
+					quic_log_error("socket poll() error %d", errno);
+					ret = -errno;
+					goto out;
+				}
+				continue;
+			}
 			if (ret < 0) {
 				quic_log_error("socket sendmsg error %d", errno);
 				ret = -errno;
