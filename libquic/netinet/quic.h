176af72c4e040b7da377cd8d59dcd095872dfd5f
Xin Long <lucien.xin@gmail.com>
2025-04-15 14:20:48 -0400

    libquic: relicense to LGPLv2.1+
    Following discussions with Stefan Metzmacher and Moritz Buhl, we've agreed
    to relicense libquic under the LGPLv2.1+ license, aligning it with the
    license used by GnuTLS. QUIC modules will remain under GPLv2+, consistent
    with the Linux kernel's licensing model.
    
    This update includes:
    
    - revised license headers in client.c, server.c, handshake.c, and
      netinet/quic.h
    
    - updated COPYING file to reflect the new license
    
    - removal of the license field from libquic.pc.in, which contains only
      trivial metadata
    
    - removal of the Linux-syscall-note from modules/include/linux/quic.h,
      as the file does not define userspace APIs.
    
    Signed-off-by: Xin Long <lucien.xin@gmail.com>

--- libquic/netinet/quic.h
+++ libquic/netinet/quic.h
@@ -1,13 +1,20 @@
-// SPDX-License-Identifier: GPL-2.0-or-later
-/* QUIC kernel implementation
- * (C) Copyright Red Hat Corp. 2023
+/*
+ * Provide APIs for QUIC handshake.
  *
- * This file is the userspace handshake part for the QUIC kernel implementation
+ * Copyright (c) 2024 Red Hat, Inc.
  *
- * Initialization/cleanup for QUIC protocol support.
+ * libquic is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
  *
- * Written or modified by:
- *    Xin Long <lucien.xin@gmail.com>
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>
  */
 
 #include <gnutls/abstract.h>
--- modules/include/linux/quic.h
+++ modules/include/linux/quic.h
@@ -1,4 +1,4 @@
-/* SPDX-License-Identifier: GPL-2.0+ WITH Linux-syscall-note */
+/* SPDX-License-Identifier: GPL-2.0-or-later */
 /* QUIC kernel implementation
  * (C) Copyright Red Hat Corp. 2023
  *
