65702ed71f93b5733a7a630e02be6f060ccfa668
Xin Long <lucien.xin@gmail.com>
2025-12-22 13:41:36 -0500

    packet: mark skb as backlog before sk_add_backlog
    Move skb_cb->backlog = 1 before adding skb to backlog, although it's
    safe to write skb_cb after adding to backlog with sk_lock.slock, as
    skb dequeue from backlog requires sk_lock.slock.
    
    This is to make some AI review tools happy.
    
    Signed-off-by: Xin Long <lucien.xin@gmail.com>

--- modules/net/quic/packet.c
+++ modules/net/quic/packet.c
@@ -603,13 +603,13 @@ int quic_packet_rcv(struct sk_buff *skb, u8 err)
 	bh_lock_sock(sk);
 	if (sock_owned_by_user(sk)) {
 		/* Socket is busy (owned by user context): queue to backlog. */
+		QUIC_SKB_CB(skb)->backlog = 1;
 		if (sk_add_backlog(sk, skb, READ_ONCE(sk->sk_rcvbuf))) {
 			QUIC_INC_STATS(net, QUIC_MIB_PKT_RCVDROP);
 			bh_unlock_sock(sk);
 			sock_put(sk);
 			goto err;
 		}
-		QUIC_SKB_CB(skb)->backlog = 1;
 		QUIC_INC_STATS(net, QUIC_MIB_PKT_RCVBACKLOGS);
 	} else {
 		/* Socket not busy: process immediately. */
