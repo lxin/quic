01553ecaeb23b222656eff86232d785848f5cb5b
Xin Long <lucien.xin@gmail.com>
2026-09-09 12:47:36 -0400

    socket: remove MIN_PMTU checks in quic_param_check_and_copy()
    QUIC transport parameters are not required to be at least MIN_PMTU.
    Remove the MIN_PMTU checks to align parameter validation with the
    QUIC RFCs.
    
    Signed-off-by: Xin Long <lucien.xin@gmail.com>

--- modules/net/quic/socket.c
+++ modules/net/quic/socket.c
@@ -2001,36 +2001,28 @@ static int quic_param_check_and_copy(struct quic_transport_param *p,
 			return -EINVAL;
 		param->max_idle_timeout = p->max_idle_timeout;
 	}
-	if (p->max_datagram_frame_size) {
-		if (p->max_datagram_frame_size < QUIC_PATH_MIN_PMTU)
-			return -EINVAL;
+	if (p->max_datagram_frame_size)
 		param->max_datagram_frame_size = p->max_datagram_frame_size;
-	}
 	if (p->max_data) {
-		if (p->max_data < QUIC_PATH_MIN_PMTU ||
-		    (!p->remote && p->max_data > (S32_MAX / 4)))
+		if (!p->remote && p->max_data > (S32_MAX / 4))
 			return -EINVAL;
 		param->max_data = p->max_data;
 	}
 	if (p->max_stream_data_bidi_local) {
-		if (p->max_stream_data_bidi_local < QUIC_PATH_MIN_PMTU ||
-		    (!p->remote &&
-		     p->max_stream_data_bidi_local > (S32_MAX / 8)))
+		if (!p->remote && p->max_stream_data_bidi_local > (S32_MAX / 8))
 			return -EINVAL;
 		param->max_stream_data_bidi_local =
 			p->max_stream_data_bidi_local;
 	}
 	if (p->max_stream_data_bidi_remote) {
-		if (p->max_stream_data_bidi_remote < QUIC_PATH_MIN_PMTU ||
-		    (!p->remote &&
-		     p->max_stream_data_bidi_remote > (S32_MAX / 8)))
+		if (!p->remote &&
+		    p->max_stream_data_bidi_remote > (S32_MAX / 8))
 			return -EINVAL;
 		param->max_stream_data_bidi_remote =
 			p->max_stream_data_bidi_remote;
 	}
 	if (p->max_stream_data_uni) {
-		if (p->max_stream_data_uni < QUIC_PATH_MIN_PMTU ||
-		    (!p->remote && p->max_stream_data_uni > (S32_MAX / 8)))
+		if (!p->remote && p->max_stream_data_uni > (S32_MAX / 8))
 			return -EINVAL;
 		param->max_stream_data_uni = p->max_stream_data_uni;
 	}
